095d3d1d45ab83943f3db920cebc580e9f7cc5c1
1gfour <hungphatpl1@gmail.com>
2022-06-19 23:16:40 +0700

    Phat commit

---
+++ HostelManagementSystem/src/java/utils/MyApplicationConstants.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author hungp
+ */
+public class MyApplicationConstants {
+
+    public class MainControllerFeatures {
+
+        public static final String HOME_PAGE = "";
+    }
+
+    public class AuthenticationFeatures {
+
+        public static final String LOGIN_CONTROLLER = "loginController";
+        public static final String HOME_PAGE = "";
+        public static final String USER_PAGE = "userPage";
+        public static final String ADMIN_PAGE = "adminPage";
+        public static final String STAFF_PAGE = "staffPage";
+        public static final String STAFF = "STAFF";
+        public static final String SEARCH_STAFF = "StaffDisplayController";
+        public static final String CREATE_STAFF = "CreateStaffController";
+        public static final String UPDATE_STAFF = "UpdateStaffController";
+        public static final String DELETE_STAFF = "DeleteStaffController";
+        public static final String US = "US";
+        public static final String AD = "AD";
+
+    }
+}
